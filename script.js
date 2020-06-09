913dd8c2e5beb06ba8819aa20499017cdc5b3cef
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-06-09 17:35:11 -0400

    Create Calculator Class

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+// create calculator class
+class Calculator {
+
+    // constructor function
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+
+    //function to clear variables
+    clear() {
+       
+    }
+
+    //function to remove single number
+    delete() {
+
+    }
+
+    //function to add digit selected to number 
+    appendNumber(number) {
+
+    }
+
+    //function to choose operation selected
+    chooseOperation(operation) {
+
+    }
+
+    //function to do the math
+    compute() {
+
+    }
+
+    //function to update display
+    updateDisplay() {
+
+    }
+}
+
 //store buttons in variables (select by data attribute)
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operations]');
@@ -6,3 +46,4 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+

3b9ab3ce45091f2a9b06f8e3105ef6d6e68c54df
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-06-09 17:27:09 -0400

    Store Buttons in Variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//store buttons in variables (select by data attribute)
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operations]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
